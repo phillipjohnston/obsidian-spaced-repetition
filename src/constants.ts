0bac9a486d082af221703fa87c1c7548119e5050
Phillip Johnston <phillip@embeddedartistry.com>
2023-08-19 11:27:05 -0700

    Adjust and expand regex constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,11 @@
 export const SCHEDULING_INFO_REGEX =
-    /^---\n((?:.*\n)*)sr-due: (.+)\nsr-interval: (\d+)\nsr-ease: (\d+)\n((?:.*\n)?)---/;
+    /^---\n((?:.*\n)*)sr-due: (.+)\nsr-interval: (\d+)\nsr-ease: ([-\d\.]+)\n((?:.*\n)?)---/;
+export const SR_DUE_REGEX =
+    /^---\n((?:.*\n)*)sr-due: (.+)\n((?:.*\n)*?)---/;
+export const SR_INTERVAL_REGEX =
+    /^---\n((?:.*\n)*)sr-interval: (\d+)\n((?:.*\n)?)---/;
+export const SR_EASE_REGEX =
+    /^---\n((?:.*\n)*)sr-ease: ([-\d\.]+)\n((?:.*\n)?)---/;
 export const YAML_FRONT_MATTER_REGEX = /^---\n((?:.*\n)*?)---/;
 
 export const MULTI_SCHEDULING_EXTRACTOR = /!([\d-]+),(\d+),(\d+)/gm;
